eb1535e66dd13dd2e6b36d94a5c54c6c45f8bb56
= <siva3060@gmail.com>
2019-05-05 17:45:49 +0530

    asdf

--- src/main/java/com/example/ABCBankDetails/repository/AccountPayeeRepository.java
+++ src/main/java/com/example/ABCBankDetails/repository/AccountPayeeRepository.java
@@ -5,7 +5,9 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import com.example.ABCBankDetails.model.CustomerPayees;
 
+import com.example.ABCBankDetails.model.AccountPayee;
 
 
 @Repository
