4f23f7d9b6773d7d6586fb0282d2c0b55932974a
= <siva3060@gmail.com>
2019-05-05 17:39:16 +0530

    asdf

--- src/main/java/com/example/ABCBankDetails/repository/AccountPayeeRepository.java
+++ src/main/java/com/example/ABCBankDetails/repository/AccountPayeeRepository.java
@@ -1,16 +1,18 @@
 package com.example.ABCBankDetails.repository;
+
+
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.example.ABCBankDetails.model.AccountPayee;
+
+
 @Repository
 public interface AccountPayeeRepository extends JpaRepository<AccountPayeeRepository, Long>{
-
-	List<AccountPayee> findByAccountNumber(Long accountNumber);
-	void deleteByAccountNoSourceAndAccountNoTarget(int accountNoSource,int accountNoTarget);
 	
+	List<CustomerPayees> findByCustomerId(Long accountNumber);
+	void deleteByAccountNoSourceAndAccountNoTarget(int accountNoSource,int accountNoTarget);	
 	AccountPayee getByAccountNoSourceAndAccountNoTarget(int accountNoSource,int accountNoTarget);
 
 }
